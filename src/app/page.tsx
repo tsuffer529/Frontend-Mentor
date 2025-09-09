4c7a2fec20be9a1a3bfffc97db77a5630f1fbc4a
foressta <tsuffer529@gmail.com>
2025-09-09 23:39:47 +0900

    refine UI design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,29 @@ import Image from "next/image";
 
 const QRCode = () => {
   return ( 
-    <div>
-      <Image
-        src="/images/image-qr-code.png"
-        alt="QRcode"
-        width={288}
-        height={288}
-        quality={100}
-        className="rounded-2xl" 
-      />
+    <Image
+      src="/images/image-qr-code.png"
+      alt="QRcode"
+      width={288}
+      height={288}
+      quality={100}
+      className="rounded-2xl" 
+    />
+  );
+}
+
+const Contents = () => {
+  return ( 
+    <div className="mb-300 px-200 space-y-200">
+      <h1 className="typo-1 text-center">
+        Improve your-front-end
+        skills by building projects
+      </h1>
+      <p className="typo-2 text-center">
+        Scan the QR code to visit Frontend
+        Mentor and take your coding skills to
+        the next level
+      </p>
     </div>
   );
 }
@@ -18,19 +32,9 @@ const QRCode = () => {
 const Home = () => {
   return ( 
     <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-white rounded-2xl pt-200 px-200 max-w-[320px]">
+      <div className="bg-white rounded-2xl space-y-300 p-200 max-w-[320px]">
         <QRCode />
-        <div className="pt-300 pb-500">
-          <div className="typo-1 text-center">
-            Improve your-front-end<br/>
-            skills by building projects
-          </div>
-          <div className="typo-2 pt-200 text-center">
-            Scan the QR code to visit Frontend<br/>
-            Mentor and take your coding skills to<br/>
-            the next level
-          </div>
-        </div>
+        <Contents />
       </div>
     </div>
   );
